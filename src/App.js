20e55f146cf6ccc1ce3306825f92257f64458a30
Badal-raj <harikeshb535@gmail.com>
2025-09-24 15:13:22 +0530

    pagination component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { useState } from 'react';
 import './App.css';
 
 import { TabForm } from './TabFile/TabForm';
+import { Pagiantion } from './Pagination/Pagination';
 
 function App() {
   const [bgColor, setBgColor] = useState("No Effect")
   return (
-    <div className="tab-form-container" style={{ background: `${bgColor === "Light" ? "aliceblue" : `${bgColor === "Dark" ? "darkseagreen" : "white"}`}` }}>
+    <>
+    {/* <div className="tab-form-container" style={{ background: `${bgColor === "Light" ? "aliceblue" : `${bgColor === "Dark" ? "darkseagreen" : "white"}`}` }}>
       <TabForm 
        setThemeColor = {setBgColor}
       />
-    </div>
+    </div> */}
+    <Pagiantion />
+    </>
   );
 }
 
---
+++ src/Pagination/Pagination.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import './Pagination.css'
+
+const page_size = 12;
+
+export const Pagiantion = () => {
+    const [allProduct, setAllProduct] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0)
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const fetchData = async () => {
+        const productData = await fetch('https://dummyjson.com/products?limit=106');
+        const all_data = await productData.json();
+        setAllProduct(all_data.products);
+    };
+
+    const total_products = allProduct.length;
+    const total_pages = Math.ceil(total_products / page_size);
+    const start = currentPage * page_size;
+    const end = start + page_size;
+
+    const handlePageSelection = (num) => {
+        setCurrentPage(num);
+    };
+
+    const handlePrevClick = () => {
+        if (currentPage !== 0) {
+            setCurrentPage((prev) => prev - 1)
+        }
+    };
+    const handleNextClick = () => {
+        if (currentPage !== total_pages - 1) {
+            setCurrentPage((prev) => prev + 1)
+        }
+    };
+
+    return (
+        <>
+            <h3 style={{ textAlign: "center" }}>Pagination</h3>
+            <div className="pagination-body">
+                <span
+                    className={currentPage === 0 ? "move-page-disable" : "move-page"}
+                    onClick={handlePrevClick}
+                >
+                    {"<<"}
+                </span>
+                {[...Array(total_pages).keys()].map((n, i) => (
+                    <span
+                        key={i}
+                        className={currentPage === i ? "selected-page" : "page-nummber"}
+                        onClick={() => handlePageSelection(n)}
+                    >
+                        {n + 1}
+                    </span>
+                )
+                )}
+                <span
+                    className={currentPage + 1 === total_pages ? "move-page-disable" : "move-page"}
+                    onClick={handleNextClick}
+                >
+                    {">>"}
+                </span>
+            </div>
+            <div className="page-container">
+                {!allProduct.length ? (<h4>No Product Found</h4>) : (
+                    allProduct.slice(start, end).map((d, index) => {
+                        return (
+                            <>
+                                <div key={index} className="harik product-container">
+                                    <img src={d.thumbnail} alt="img" className="img-style" />
+                                    <span className="title">{d.title}</span>
+                                </div>
+                            </>
+                        )
+                    })
+                )}
+            </div>
+        </>
+    )
+}
